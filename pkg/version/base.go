cb201ce9d819c634826286f445b79fa3622621d6
Zach Loafman <zml@google.com>
2015-07-07 09:59:47 -0700

    Kubernetes version v9999.9999.0

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -35,9 +35,9 @@ package version
 
 var (
 	// TODO: Deprecate gitMajor and gitMinor, use only gitVersion instead.
-	gitMajor     string = "0"              // major version, always numeric
-	gitMinor     string = "20.2+"          // minor version, numeric possibly followed by "+"
-	gitVersion   string = "v0.20.2-dev"    // version from git, output of $(git describe)
+	gitMajor     string = "9999"           // major version, always numeric
+	gitMinor     string = "9999.0"         // minor version, numeric possibly followed by "+"
+	gitVersion   string = "v9999.9999.0"   // version from git, output of $(git describe)
 	gitCommit    string = ""               // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState string = "not a git tree" // state of git tree, either "clean" or "dirty"
 )
